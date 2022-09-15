97153a48642a2e8e67f24f50b3d5c17448bfb444
Mike W <mwwoody9@gmail.com>
2022-09-15 05:05:03 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*_isupper - checks for uppercase character.
+*@c: input
+*Return: 0 for other and 1 for uppercase
+*Description: a function that checks for uppercase character.
+*/
+
+int _isupper(int c)
+{
+	int upper[] = {'A','B','C','D','E','F','G','H','I','G','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	int lower[] = {'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+	int arrayCheck = 0;
+
+	do{
+		if (c == upper[arrayCheck])
+		{
+			 return (1);
+		}
+		else if (c == lower[arrayCheck])
+		{
+			return (0);
+		}
+		else
+		{
+			arrayCheck++;
+		}
+	}while (arrayCheck <= 25);
+}

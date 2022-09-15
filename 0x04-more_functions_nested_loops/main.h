6573e2887bb567adffdd1e7cf6c68b057a28ce9c
Mike W <mwwoody9@gmail.com>
2022-09-15 07:50:05 -0700

    Update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,70 +11,17 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/*
-*
-*/
-int _isupper(int c)
-{
-	int upper[] = {'A','B','C','D','E','F','G','H','I','G','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-	int lower[] = {'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	int output;
-	int arrayCheck = 0;
-
-	do{
-		if (c == upper[arrayCheck])
-		{
-			output = 1;
-		}
-		else if (c == lower[arrayCheck])
-		{
-			output = 0;
-		}
-		else
-		{
-			arrayCheck++;
-		}
-	}while (arrayCheck <= 25);
-	return (output);
-}
+int _putchar(char c);
 
 /*
 *
 */
-int _isdigit(int c)
-{
-	int upper[] = {'A','B','C','D','E','F','G','H','I','G','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-	int lower[] = {'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	char d = '0';
-	int output;
-	int arrayCheck = 0;
+int _isupper(int c);
 
-	do
-	{
-		if (c == upper[arrayCheck])
-		{
-			output = 0;
-		}
-		else if (c == lower[arrayCheck])
-		{
-			output = 0;
-		}
-		else if (c == d)
-		{
-			output = 1;
-		}
-		else
-		{
-			arrayCheck++;
-			d++;
-		}
-	}while (arrayCheck <= 25);
-	return (output);
-}
+/*
+*
+*/
+int _isdigit(int c);
 
 /*
 *

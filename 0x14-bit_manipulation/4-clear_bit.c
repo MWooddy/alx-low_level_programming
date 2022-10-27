3b12fa352fd91ace86e7114136b644b6ed79a1d6
Mike W <mwwoody9@gmail.com>
2022-10-27 10:21:51 -0700

    Update

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* clear_bit - sets the value of a bit to 1 at a given index.
+* @n: pointer
+* @index: index
+* Return: 1 if it worked or -1 if an error occured
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 32)
+	{
+		*n = *n & ~(1 << index);
+		return (1);
+	}
+	return (-1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* flip_bits - count the number of bits you would need to flip
+* to get from one number to another.
+* @n: 1st num to compare
+* @m: 2nd num to compare
+* Return: number of bits you would need to flip
+* to get from one number to another.
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num;
+	unsigned int bits;
+
+	bits = 0;
+	num = n ^ m;
+
+	while (num != 0)
+	{
+		bits += num & 1;
+		num = num >> 1;
+	}
+	return (bits);
+}
